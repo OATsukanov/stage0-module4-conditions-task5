1b4c62c266ee6087b24d8b9d13188962c4214a86
oleg <tsukanov.regula@gmail.com>
2023-01-10 20:38:18 +0500

    first try

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+
+        int residue = year%4;
+
+        boolean isLastYearOfCentury = year%100 == 0;
+        boolean isMultipleOf400 = year%400 == 0;
+
+        switch (residue) {
+            default:
+                System.out.println("not leap");
+                break;
+            case (0):
+                if (isLastYearOfCentury == false) {
+                    System.out.println("leap");
+                } else if (isMultipleOf400 == true) {
+                    System.out.println("leap");
+                } else {
+                    System.out.println("not leap");
+                }
+                break;
+        }
+
     }
 }
